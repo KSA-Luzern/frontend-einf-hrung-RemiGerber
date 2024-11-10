4d30517172dedc8592003358bdb095d5a1cd68b1
RemiGerber <remigerber@protonmail.com>
2024-11-10 22:53:09 +0100

    add emoji with custom text input functionality

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
- 
+let lefteyestate = false;
+function go(){
+    let text = document.getElementById("textbox").value;
+    lefteyestate = !lefteyestate;
+    if(lefteyestate){
+        document.getElementById("lefteye").innerHTML = text;
+        document.getElementById("lefteye").style.fontSize = "40";
+    }
+    else{
+        document.getElementById("lefteye").innerHTML = "O";
+        document.getElementById("lefteye").style.fontSize = "50";
+
+    }
+}
 
